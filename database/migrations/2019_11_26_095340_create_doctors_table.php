5ca6efa5bbc04b4e4263298ef5715b6233758759
Nazrul Islam <info.n121@gmail.com>
2019-11-26 23:18:18 +0600

    inserting new laravel project , proposed for admitdoctors

---
+++ database/migrations/2019_11_26_095340_create_doctors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('reg');
+            $table->string('photo');
+            $table->text('education');
+            $table->enum('is_active', ['1', '0'])->default('1');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
